392dbe53f88ba7cf714c17a8bb1eae13897408b2
userJFD <joelfregno@outlook.com>
2025-02-02 13:18:47 +0100

    completed about section

---
+++ src/components/About/About.tsx
@@ -0,0 +1,43 @@
+import { getImageUrl } from "../../utils";
+import styles from "./About.module.css";
+
+export const About = () => {
+  return (
+    <section className={styles.container} id="about">
+      <h2 className={styles.title}>About</h2>
+      <div className={styles.content}>
+        <img
+          src={getImageUrl("about/aboutImage.png")}
+          alt="Me sitting with a laptop"
+          className={styles.aboutImage}
+        />
+        <ul className={styles.aboutItems}>
+          <li className={styles.aboutItem}>
+            <img src={getImageUrl("about/cursorIcon.png")} alt="Cursor icon" />
+            <div className={styles.aboutItemText}>
+              <h3>Frontend Developer</h3>
+              <p>Experience in building responsive and optimized sites</p>
+            </div>
+          </li>
+          <li className={styles.aboutItem}>
+            <img src={getImageUrl("about/serverIcon.png")} alt="Server icon" />
+            <div className={styles.aboutItemText}>
+              <h3>Backend Developer</h3>
+              <p>
+                Experience in developing fast and optimized backend systems and
+                APIs
+              </p>
+            </div>
+          </li>
+          <li className={styles.aboutItem}>
+            <img src={getImageUrl("about/uiIcon.png")} alt="UI icon" />
+            <div className={styles.aboutItemText}>
+              <h3>UI Designer</h3>
+              <p>Designed multiple landing pages and created design systems</p>
+            </div>
+          </li>
+        </ul>
+      </div>
+    </section>
+  );
+};
